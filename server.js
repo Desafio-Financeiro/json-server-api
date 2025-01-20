5ac7f3617da6b80e04a6c3e57eaa3e506ba4d1b4
Cris <kristiano.gon@gmail.com>
2025-01-20 18:41:38 -0300

    update api

--- server.js
+++ server.js
@@ -3,7 +3,25 @@ const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 
+server.get("/total-balance", (req, res) => {
+  const userId = req.params.userId;
+  const db = router.db;
+
+  const transactions = db.get("transactions").value();
+
+  const total = transactions
+    .filter((t) => t.userId === userId)
+    .reduce((acc, curr) => {
+      if (curr.type === "saque") return acc - curr.value;
+
+      return acc + curr.value;
+    }, 0);
+
+  return res.json({ total });
+});
+
 server.use(middlewares);
+
 server.use(router);
 
 server.listen(8080, () => {
