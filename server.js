86c21008e517be485b7aadb346336959fff03d3c
Cris <kristiano.gon@gmail.com>
2025-01-21 14:00:28 -0300

    chore: api config

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 const cors = require("cors");
 
 const fs = require("fs");
 const path = require("path");
-const db = JSON.parse(fs.readFileSync(path.join("db.json")));
+const filePath = path.join("db.json");
+const data = fs.readFileSync(filePath, "utf-8");
+const db = JSON.parse(data);
+const router = jsonServer.router(db);
 
 server.use(cors());
 
