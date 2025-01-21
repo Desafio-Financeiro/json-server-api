3c41c1aaddb486050c151e038f610bd93de90695
Cris <kristiano.gon@gmail.com>
2025-01-21 12:40:23 -0300

    chore: api config

--- server.js
+++ server.js
@@ -5,6 +5,23 @@ const middlewares = jsonServer.defaults();
 const cors = require("cors");
 
 server.use(cors());
+
+server.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*"); // Permite qualquer origem
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, OPTIONS"
+  ); // Métodos permitidos
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization"); // Cabeçalhos permitidos
+
+  // Trata requisições OPTIONS (preflight)
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(204); // Responde com status "No Content"
+  }
+
+  next(); // Continua para a próxima rota ou middleware
+});
+
 server.use(middlewares);
 
 server.get("/balance", (req, res) => {
